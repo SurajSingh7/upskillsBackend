39a09276bf7ab1ffa037f2e65848868434e95f5a
SurajSingh7 <220suraj@gmail.com>
2023-10-03 17:44:57 +0530

    nodemailer

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,9 +1,11 @@
 const nodemailer = require("nodemailer");
+require('dotenv').config()
 
 const mailSender = async (email, title, body) => {
     try{
             let transporter = nodemailer.createTransport({
                 host:process.env.MAIL_HOST,
+                port:587,
                 auth:{
                     user: process.env.MAIL_USER,
                     pass: process.env.MAIL_PASS,
@@ -12,7 +14,7 @@ const mailSender = async (email, title, body) => {
 
 
             let info = await transporter.sendMail({
-                from: 'StudyNotion || CodeHelp - by Babbar',
+                from: 'Upskills',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
